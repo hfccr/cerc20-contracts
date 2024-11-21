db2a9c0315dd5b5bfa948ac9a01e49d59a1690b7
hfccr <hfccr@outlook.com>
2024-11-21 12:26:58 +0530

    Rename ERC20 MyToken to USDC

--- contracts/USDC.sol
+++ contracts/USDC.sol
@@ -4,11 +4,18 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract MyToken is ERC20 {
-    constructor() ERC20("MyToken", "MTK") {
-        _mint(msg.sender, 1000);
-    }
+contract USDC is ERC20 {
+    constructor() ERC20("USD Coin", "USDC") {}
+
     function decimals() public pure override returns (uint8) {
         return 6;
+    }
+
+    function mint(uint256 amount) public {
+        _mint(msg.sender, amount);
+    }
+
+    function burn(uint256 amount) public {
+        _burn(msg.sender, amount);
     }
 }
